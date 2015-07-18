5e177a17148a736b023b4c2a1fd72ba0aa32cca0
Rafal Gorzkowski <rafalgorzkowski@gmail.com>
2015-07-18 22:54:58 +0200

    Web app example - hello world

---
+++ helloworld/src/main/java/info/gorzkowski/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package info.gorzkowski.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
