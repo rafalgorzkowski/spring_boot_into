3476475c1dd48474040fd1169c6d78a6a2f7fa26
Rafal Gorzkowski <rafalgorzkowski@gmail.com>
2015-07-19 04:13:27 +0200

    Application properties

--- demo/src/main/java/info/gorzkowski/demo/DemoApplication.java
+++ demo/src/main/java/info/gorzkowski/demo/DemoApplication.java
@@ -1,12 +1,21 @@
 package info.gorzkowski.demo;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createWebServer("-tcpPort", "8082", "-webAllowOthers");
     }
 }
