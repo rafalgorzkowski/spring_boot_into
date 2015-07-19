c8a43deb46633237ae5c79a4581917872c2560d6
Rafal Gorzkowski <rafalgorzkowski@gmail.com>
2015-07-19 03:09:47 +0200

    Thymeleaf - init config

---
+++ demo/src/main/java/info/gorzkowski/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package info.gorzkowski.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class HomeController {
+
+    @RequestMapping("/")
+    String index() {
+        return "home    ";
+    }
+}
