c24d39302c4f63af6ac4b2795fb6ac02d20187e3
Rafal Gorzkowski <rafalgorzkowski@gmail.com>
2015-08-03 00:13:00 +0200

    Profiles - loggers

--- demo/src/main/java/info/gorzkowski/demo/controller/HomeController.java
+++ demo/src/main/java/info/gorzkowski/demo/controller/HomeController.java
@@ -1,13 +1,22 @@
 package info.gorzkowski.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 class HomeController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping("/")
     String index() {
+
+        // TODO :) Run me with  --spring.profiles.active
+        LOGGER.debug("Dev log");
+        LOGGER.info("Prod log");
+
         return "home";
     }
 }
