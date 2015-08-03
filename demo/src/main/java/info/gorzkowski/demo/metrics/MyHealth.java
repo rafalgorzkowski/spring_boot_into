baa516610553569f4c90e16ed1df6aa5db77725d
Rafal Gorzkowski <rafal.gorzkowski@gtech.com>
2015-08-04 00:31:50 +0200

    Actuator - custom health indicator

---
+++ demo/src/main/java/info/gorzkowski/demo/metrics/MyHealth.java
@@ -0,0 +1,21 @@
+package info.gorzkowski.demo.metrics;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyHealth implements HealthIndicator {
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check() {
+        return -1;
+    }
+}
