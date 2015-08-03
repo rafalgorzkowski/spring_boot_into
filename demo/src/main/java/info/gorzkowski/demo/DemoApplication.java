e03358348353f816051d109b6aeec41ae89a0fb3
Rafal Gorzkowski <rafal.gorzkowski@gtech.com>
2015-08-04 01:12:50 +0200

    Rest example

--- demo/src/main/java/info/gorzkowski/demo/DemoApplication.java
+++ demo/src/main/java/info/gorzkowski/demo/DemoApplication.java
@@ -4,10 +4,13 @@ import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -17,5 +20,36 @@ public class DemoApplication {
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2Server() throws SQLException {
         return Server.createWebServer("-tcpPort", "8082", "-webAllowOthers");
+    }
+
+    @RequestMapping("/user")
+    public User rc() {
+        return new User("Adam", 10);
+    }
+
+    private class User {
+        private String name;
+        private Integer age;
+
+        public User(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
     }
 }
